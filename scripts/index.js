88ea23c6fa5311c67aa5bd1efed33eed45102c94
Anton Provolotsky <lnza23153@gmail.com>
2023-04-26 16:43:43 +1000

    Mesto three try

--- scripts/index.js
+++ scripts/index.js
@@ -6,28 +6,24 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const nameProfile = document.querySelector('.profile__title');
 const jobProfile = document.querySelector('.profile__subtitle');
-const saveButton = document.querySelector('.popup__save')
 
-const handleEditButtonClick = () => {
+const handleOpenPopup = () => {
     popup.classList.add('popup_opened');
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent ;
 }
 
-const handleCloseButtonClick = () => {
-    popup.classList.remove('popup_opened')
+const handleClosePopup = () => {
+    popup.classList.remove('popup_opened');
 }
-const handleSaveButtonClick = () => {
-    popup.classList.remove('popup_opened')
-}
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value ;
     jobProfile.textContent = jobInput.value ;
+    handleClosePopup();
+   
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-editButton.addEventListener('click', handleEditButtonClick );
-closeButton.addEventListener('click', handleCloseButtonClick );
-saveButton.addEventListener('click', handleSaveButtonClick);
+editButton.addEventListener('click', handleOpenPopup );
+closeButton.addEventListener('click', handleClosePopup );
